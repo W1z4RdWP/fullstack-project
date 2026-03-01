896a5659d514ac7c0bcb04b96af200edeff5471a
w1z4rdWP <headgenrekty@gmail.com>
2026-03-01 17:00:08 +0400

    Обновлен компонент Footer

--- backend/myproject/api/views.py
+++ backend/myproject/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework import status
 
 from blog.models import Post
@@ -9,19 +10,19 @@ from .serializers import PostSerializer
 
 
 @api_view(["GET"])
-def get_all_posts(request):
+def get_all_posts(request: Request) -> Response:
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
 
-def register_user(request):
+def register_user(request: HttpRequest) -> HttpResponse:
     headers = {'My-SecretCode': 12345}
     return HttpResponse("<h1>Register user</h1>", status=200, headers=headers)
 
 
 
 @api_view(["GET", "POST"])
-def create_post(request):
+def create_post(request: Request) -> Response:
     """CREATE"""
     if request.method == 'POST':
         serializer = PostSerializer(request.data)
@@ -29,4 +30,3 @@ def create_post(request):
         return Response({"status": "Post Created!", "pk": post.id}, status=status.HTTP_201_CREATED)
     else:
         return Response({"status": "This was GET REQUEST"})
-    
--- backend/myproject/blog/views.py
+++ backend/myproject/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import Post
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 
 
-def create_post(request):
+def create_post(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
     """CREATE"""
     if request.method == 'POST':
         post = Post()
@@ -13,13 +13,13 @@ def create_post(request):
 
 
 
-def get_all_posts(request):
+def get_all_posts(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
     """READ"""
     posts = Post.objects.all()
     return render(request, 'blog/index.html', {'posts': posts})
 
 
-def read_post(request, pk):
+def read_post(request: HttpRequest, pk: int) -> HttpResponse | HttpResponseRedirect:
     """READ"""
     try:
         post = Post.objects.get(id=pk)
@@ -28,7 +28,7 @@ def read_post(request, pk):
         return HttpResponseNotFound("/blog/get_posts/")
 
 
-def update_post(request, pk):
+def update_post(request: HttpRequest, pk: int) -> HttpResponse | HttpResponseRedirect:
     """UPDATE"""
     post = Post.objects.get(id=pk)
     if request.method == "POST":
@@ -39,14 +39,13 @@ def update_post(request, pk):
         return render(request, "blog/edit.html", {"post": post})
 
 
-def delete_post(request, pk):
+def delete_post(request: HttpRequest, pk: int) -> HttpResponseRedirect | HttpResponseNotFound:
     """DELETE"""
     try:
         post = Post.objects.get(id=pk)
         if request.method == "GET":
             post.delete()
             return HttpResponseRedirect("/blog/get_posts/")
+        return HttpResponseNotFound("/blog/get_posts/")
     except Post.DoesNotExist() as e:
         return HttpResponseNotFound("/blog/get_posts/")
-
-
